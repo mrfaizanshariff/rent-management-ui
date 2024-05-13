bfd11c499116d6f2fd31bd7b93ede2cd7299fc46
Mohammed Faizan Shariff <71890836+mrfaizanshariff@users.noreply.github.com>
2024-05-13 21:15:36 +0530

    phase 1, material and firebase setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment.prod';
+import { RegisterComponentComponent } from './components/auth/register-component/register-component.component';
+import { LoginComponentComponent } from './components/auth/login-component/login-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCj1nu5_aOlwmu8pOSZ6-qYz0cOOm05Pkw",
+    authDomain: "rental-management-app-e8cbb.firebaseapp.com",
+    projectId: "rental-management-app-e8cbb",
+    storageBucket: "rental-management-app-e8cbb.appspot.com",
+    messagingSenderId: "691945530275",
+    appId: "1:691945530275:web:f48852fc3b521a2b3cb016",
+    measurementId: "G-GBTW4XZT36"
+  }
 };
